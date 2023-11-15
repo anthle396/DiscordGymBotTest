3f87ab3a1d645c00080fff1d0a6f69e105dd4139
Aaron <aaronmai4@gmail.com>
2023-11-15 01:05:37 -0800

    cleaned comments

--- functions/leaderboard.py
+++ functions/leaderboard.py
@@ -7,9 +7,6 @@
 # Last Updated: 8/18/2023 Aaron, Anthony                                  #
 ###########################################################################
 from functions import parse_PRlist_txt as p
-# import numpy as np
-# import pandas as panda
-# import matplotlib.pyplot as plt
   
 def create_leaderboard(name):
   user_dict = p.create_user_dictionary()
@@ -17,12 +14,9 @@ def create_leaderboard(name):
   name = p.capitalize_firstletter(name)
   for user, data in user_dict.items():
     print(data[name])
-  #sorts the dictionary items by the 1st index of the tuple of the value of bench, reverse=True is most weight to       lowest
-  
-  # sorted_dict = sorted(user_dict.items(), key=bench_key, reverse=True)
-  #print(sorted_dict.items())
+
+  #sorts the dictionary items by the 1st index of the tuple of the value of bench, reverse=True is most weight to lowest
   sorted_dict = sorted(user_dict.items(), key=lambda x: float(x[1][name]), reverse=True)
-  #sorted_list = [(user, stats) for user, stats in sorted_dict]
   return sorted_dict
 
 def leaderboard(name):
